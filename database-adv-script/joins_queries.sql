3d696809f9755097da276ca76310dc74498bda70
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-07-07 18:14:27 -0500

    added the order by property_id to left join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,12 +1,21 @@
+C
 -- 1. INNER JOIN: bookings with user details
 SELECT b.*, u.first_name, u.last_name, u.email
 FROM booking b
 INNER JOIN app_user u ON b.user_id = u.user_id;
 
 -- 2. LEFT JOIN: all properties and any associated reviews
-SELECT p.*, r.rating, r.comment, r.created_at
-FROM property p
-LEFT JOIN review r ON p.property_id = r.property_id;
+select 
+p.property_id,
+p.name,
+r.review_id,
+r.rating,
+r.comment
+
+from property p 
+left join review r p.proprty_id = r.property_id
+order by p.property_id
+
 
 -- 3. FULL OUTER JOIN: all users and bookings (even if unrelated)
 SELECT u.user_id, u.first_name, b.booking_id, b.start_date
